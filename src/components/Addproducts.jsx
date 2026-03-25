d7a9ce05f45df994e263bfa4f456f0d028b5bed5
coder43cell <adrianmuita49@gmail.com>
2026-03-25 10:41:09 +0300

    second commit

--- src/components/Addproducts.jsx
+++ src/components/Addproducts.jsx
@@ -9,13 +9,13 @@ const Addproduct=()=>{
     const[product_cost,setProductCost]=useState("")
     const[product_photo,setProductPhoto]=useState("")
     // define 3 States for posting data 
-    const[loading,setloading]=useState("")
+    const[loading,setLoading]=useState("")
     const[success,setSuccess]=useState("")
     const[error,setError]=useState("")
     // function to handle submit 
     const handlesubmit=async(e)=>{
        e.preventDefault()
-       setloading("please wait...")
+       setLoading("please wait...")
     //    create empty digital envelope to store user inputs 
     const formdata=new FormData()
     // append/add
@@ -27,7 +27,7 @@ const Addproduct=()=>{
     try{
         const response=await axios.post("https://adrianhiggs.alwaysdata.net/api/addproduct",formdata)
         setSuccess(response.data.message)
-        setloading("")
+        setLoading("")
         if(response.data.user){
             localStorage.setItem("user",JSON.stringify(response.data.user))
             navigate("/")
@@ -37,6 +37,8 @@ const Addproduct=()=>{
         }
     }
     catch(error){
+         setError(error.message)
+            setLoading("")
         
     }
 
--- src/components/Getproduct.jsx
+++ src/components/Getproduct.jsx
@@ -19,6 +19,8 @@ const Getproduct=()=>{
             setProducts(response.data)
         }
         catch(error){
+             setError(error.message)
+            setLoading("")
 
         }
     }
